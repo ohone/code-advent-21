4f2d388e64873054c13c9ea63927fb05895f40b9
eoghan <eoghan.firth@gmail.com>
2021-12-01 16:17:11 +0000

    typo

--- 1/1/index.ts
+++ 1/1/index.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs'
 
-const numbers = fs.readFileSync('unput', 'utf8').split('\n');
+const numbers = fs.readFileSync('input', 'utf8').split('\n');
 let count = 0;
 for (let current = 1; current < numbers.length; current++) {
     const element = +numbers[current];
